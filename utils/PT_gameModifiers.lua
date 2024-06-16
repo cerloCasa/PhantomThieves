23595a7b93ca65c247c0dcbef70f0aad8b85f583
cerloCasa <cerlo.casa@gmail.com>
2024-06-16 03:50:30 +0200

    functions that modify the game are held here

--- utils/PT_gameModifiers.lua
+++ utils/PT_gameModifiers.lua
@@ -2,9 +2,9 @@ PT_GAMEMODIFIERS = {}
 
 -- MODIFICA LA FUNZIONE use_card CHE SI ATTIVA QUANDO UN CONSUMABILE VIENE USATO
  
-local G_FUNCS_use_card_ref = G.FUNCS.use_card
+local vanilla_G_FUNCS_use_card = G.FUNCS.use_card
 G.FUNCS.use_card = function(e,mute,nosave)
-    G_FUNCS_use_card_ref(e,mute,nosave)
+    vanilla_G_FUNCS_use_card(e,mute,nosave)
     local card = e.config.ref_table
     if card.ability.consumeable then
         if (card.ability.set == 'Tarot') then
